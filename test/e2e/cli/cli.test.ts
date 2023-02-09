75a6cdf7f0b157fae534ea74926298dc6f7494d0
Shubh Bapna <sbapna@redhat.com>
2023-02-08 19:47:41 -0500

    improve cli custom tests

--- test/e2e/cli/cli.test.ts
+++ test/e2e/cli/cli.test.ts
@@ -1,6 +1,6 @@
 import { Act } from "@kie/act-js";
 import { EventJSON } from "@kie/act-js/build/src/action-event/action-event.types";
-import { readFileSync } from "fs";
+import { cpSync, mkdirSync, readFileSync, rmSync } from "fs";
 import path from "path";
 import { logActOutput } from "../helper/logger";
 
@@ -9,12 +9,27 @@ type TestCommand = {
   cmd: string;
   env?: Record<string, string>;
   eventPayload?: EventJSON;
+  shouldFail?: boolean;
 };
 
 describe("test custom cli e2e commands", () => {
   const testCases: TestCommand[] = JSON.parse(
     readFileSync(path.join(__dirname, "tests.json"), "utf8")
   ) as TestCommand[];
+
+  const tmpFolder = path.join(__dirname, "tmp");
+  
+  beforeAll(() => {
+    mkdirSync(tmpFolder);
+  });
+
+  afterEach(() => {
+    rmSync(tmpFolder, { recursive: true });
+  });
+  
+  beforeEach(() => {
+    cpSync(path.join(__dirname, "cli.yaml"), path.join(tmpFolder, "cli.yaml"));
+  });
 
   for (const testCase of testCases) {
     test(testCase.name, async () => {
@@ -32,7 +47,7 @@ describe("test custom cli e2e commands", () => {
 
       const result = await act.runEvent("pull_request", {
         ...logActOutput(`${testCase.name}.log`),
-        workflowFile: path.join(__dirname),
+        workflowFile: tmpFolder,
         mockSteps: {
           build: [
             {
@@ -43,9 +58,9 @@ describe("test custom cli e2e commands", () => {
         },
       });
 
-      // console.log(result);
-
-      expect(true).toBe(true);
+      expect(result.length).toBe(7);
+      expect(result[6].name).toBe("Main Execute build-chain");
+      expect(result[6].status).toBe(testCase.shouldFail ? 1 : 0);
     });
   }
 });
